9454e50ba0d9c14f421c147e509313e498793200
Jonas Schmidt <Jonas.Schmidt@exprogroup.com>
2025-07-30 14:41:07 -0500

    Removed excess code. Do not forget to compile into x64 DLL when compiling.

--- timestampConversion.cpp
+++ timestampConversion.cpp
@@ -1,14 +1,15 @@
 /* TODO: 
-    - implement the chrono library in place of time EVERYWHERE
-    - enforce C++20 compilation ?
+
+    - C++20 compilation ?
 */
-#include <iostream>
-#include <fstream>
+#include <iostream> // this can probably be removed before final release
+#include <fstream>  // this can probably be removed before final release
+
 #include <cstdint>
 #include <cmath>
-#include <time.h>
-
 #include <chrono>
+
+extern "C"{
 
 // define the LabVIEW timestamp struct (64-bit seconds, 64-bit fraction)
 struct lvTimestamp
@@ -17,74 +18,11 @@ struct lvTimestamp
     int64_t seconds;     // seconds since 01/01/1904 00:00:00 (signed 64-bit)
 };
 
-// TODO: implementation
-struct dtTimestamp{
-    time_t time;
-    uint64_t fracSecs;  // fractionsl seconds for extended accuracy
-};
-
-// function to log the timestamp values into a text file
-extern "C" __declspec(dllexport) void LogTimestampValues(const lvTimestamp* timestamp)
-{
-    // open a text file for logging
-    std::ofstream logFile("C:/Logs/log.txt", std::ios::app);  // open in append mode
-    
-    if (!logFile)
-    {
-        std::cerr << "Error opening file for logging!" << std::endl;
-        return;
-    }
-
-    // log the seconds and fraction values
-    logFile << "Fraction: " << timestamp->fraction << std::endl;
-    logFile << "Seconds: " << timestamp->seconds << std::endl;
-
-    // close the file
-    logFile.close();
-}
-
-// TODO: try/catch on dates before the UNIX epoch
-// TODO: include fractional seconds
-// TODO: encorporate dtTimestamp struct
-extern "C" __declspec(dllexport) void LogTimestampValuesReadable(const lvTimestamp* timestamp)
-{
-    const int LV_EPOCH_DIFF = 2082844800;
-    const uint64_t TWO_POW_64 = ~0ULL;  // maximum unit_64 value
-
-    int add_seconds = timestamp->seconds;
-    //int64_t add_fracion = timestamp->fraction;
-
-    //time_t UNIXts = (add_seconds + lvEpochDiff);
-    time_t UNIXts = (time_t)(add_seconds - LV_EPOCH_DIFF);
-
-    // get milliseconds
-    double fractionalSeconds = static_cast<double>(timestamp->fraction) / TWO_POW_64;
-    uint16_t milliseconds = std::round(fractionalSeconds * 1000);
-
-    // open a text file for logging
-    std::ofstream logFile("C:/Logs/log.txt", std::ios::app);  // open in append mode
-    
-    if (!logFile)
-    {
-        std::cerr << "Error opening file for logging!" << std::endl;
-        return;
-    }
-
-    // convert the timestamp to something readable
-    char* timeStr = ctime(&UNIXts);
-
-    if (timeStr != nullptr){
-        logFile << timeStr << "and " << milliseconds << " milliseconds";
-        logFile << add_seconds << std::endl;
-        logFile << milliseconds << std::endl;
-        logFile << "---" << std::endl;
-    }
-    else
-        logFile << "[Invalid timestamp]" << std::endl;
-}
-
-extern "C" __declspec(dllexport) void LogTimestampValuesReadableWChrono(const lvTimestamp* timestamp)
-{
+// TODO: factor in timezones
+// seems to work for times even before UNIX epoch
+// seems to work for times even before LabVIEW epoch
+__declspec(dllexport)
+void LogTimestampValuesReadableWChrono(const lvTimestamp* timestamp){
     using namespace std::chrono;
 
     const year_month_day LV_EPOCH = {January/01/1904};
@@ -109,11 +47,14 @@ extern "C" __declspec(dllexport) void LogTimestampValuesReadableWChrono(const lv
     }
 
     logFile << log_time << std::endl;
-    logFile << add_seconds << std::endl;
-    logFile << add_milliseconds << std::endl;
+    //logFile << add_seconds << std::endl;
+    //logFile << add_milliseconds << std::endl;
     logFile << "---" << std::endl;
 }
 
+} // extern "C"
+
+/*
 // (for testing functions)
 int main()
 {
@@ -125,9 +66,21 @@ int main()
     //LogTimestampValues(labviewTimestampPtr);
 
     // convert the timestamp to a UNIX timestamp, then as a human-readable string
-    LogTimestampValuesReadable(labviewTimestampPtr);
+    // LogTimestampValuesReadable(labviewTimestampPtr);
+    LogTimestampValuesReadableWChrono(labviewTimestampPtr);
+
+    // seconds: 7862400, Fraction: 8411715297611555536 (04/01/1904 00:00:00.456)
+    labviewTimestamp = {8411715297611555536, 7862400};
+    labviewTimestampPtr = &labviewTimestamp;
+    LogTimestampValuesReadableWChrono(labviewTimestampPtr);
+
+    // seconds: -60, Fraction: 8411715297611555536 (12/31/1903 23:59:00.456)
+    labviewTimestamp = {8411715297611555536, -60};
+    labviewTimestampPtr = &labviewTimestamp;
     LogTimestampValuesReadableWChrono(labviewTimestampPtr);
 
     return 0;
 }
+*/
+
 
